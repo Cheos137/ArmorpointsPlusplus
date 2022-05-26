316391b9be44447b4112fbd7b8fa114de39d823d
Cheos <cheoscrafter@gmail.com>
2022-05-26 10:49:51 +0200

    fix debug text to be less confusing

--- core/src/main/java/dev/cheos/armorpointspp/core/render/DebugTextComponent.java
+++ core/src/main/java/dev/cheos/armorpointspp/core/render/DebugTextComponent.java
@@ -1,16 +1,14 @@
 package dev.cheos.armorpointspp.core.render;
 
-import dev.cheos.armorpointspp.core.IRenderComponent;
-import dev.cheos.armorpointspp.core.RenderContext;
-import dev.cheos.armorpointspp.core.RenderableText;
+import dev.cheos.armorpointspp.core.*;
 import dev.cheos.armorpointspp.core.RenderableText.Alignment;
 import dev.cheos.armorpointspp.core.adapter.IConfig.BooleanOption;
 import dev.cheos.armorpointspp.core.adapter.IConfig.IntegerOption;
 
 public class DebugTextComponent implements IRenderComponent {
-	private static RenderableText right  = new RenderableText("this should be left and red with no shadow").withAlignment(Alignment.LEFT  ).withColor(0xff0000).withShadow(false);
-	private static RenderableText center = new RenderableText("this should be centered and green"         ).withAlignment(Alignment.CENTER).withColor(0x00ff00);
-	private static RenderableText left   = new RenderableText("this should be right and blue"             ).withAlignment(Alignment.RIGHT ).withColor(0x0000ff);
+	private static RenderableText right  = new RenderableText("this should be left aligned and red with no shadow").withAlignment(Alignment.LEFT  ).withColor(0xff0000).withShadow(false);
+	private static RenderableText center = new RenderableText("this should be centered and green"                 ).withAlignment(Alignment.CENTER).withColor(0x00ff00);
+	private static RenderableText left   = new RenderableText("this should be right aligned and blue"             ).withAlignment(Alignment.RIGHT ).withColor(0x0000ff);
 	
 	@Override
 	public boolean render(RenderContext ctx) {
