9e193a2403cd68ca244fa354a5f333d958f90e48
Cheos <cheoscrafter@gmail.com>
2023-01-15 14:34:34 +0100

    retain 1.19.2 compatibility

---
+++ fabric-1.19.2/src/main/java/dev/cheos/armorpointspp/impl/ItemStackImpl.java
@@ -0,0 +1,27 @@
+package dev.cheos.armorpointspp.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.cheos.armorpointspp.core.adapter.IItemStack;
+import net.minecraft.world.item.ItemStack;
+
+public class ItemStackImpl implements IItemStack {
+	private final ItemStack stack;
+	
+	public ItemStackImpl(ItemStack stack) {
+		this.stack = stack;
+	}
+	
+	@Override
+	public Object getStack() {
+		return this.stack;
+	}
+	
+	public static Iterable<IItemStack> wrap(Iterable<ItemStack> stacks) {
+		List<IItemStack> out = new ArrayList<>();
+		for (ItemStack stack : stacks)
+			out.add(new ItemStackImpl(stack));
+		return out;
+	}
+}
