ba602843fb535c7900551ac99733b33489e38005
Cheos <cheoscrafter@gmail.com>
2023-04-02 14:00:50 +0200

    fork fabric 1.19.3

---
+++ fabric-1.19.3/src/main/java/dev/cheos/armorpointspp/compat/Compat.java
@@ -0,0 +1,40 @@
+package dev.cheos.armorpointspp.compat;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+public class Compat {
+	private static boolean libhud, fabricApi, appleskin, spectrum, raised, bewitchment;
+	
+	public static void init() {
+		libhud = FabricLoader.getInstance().isModLoaded("libhud");
+		fabricApi = FabricLoader.getInstance().isModLoaded("fabric");
+		appleskin = FabricLoader.getInstance().isModLoaded("appleskin");
+		spectrum = FabricLoader.getInstance().isModLoaded("spectrum");
+		raised = FabricLoader.getInstance().isModLoaded("raised");
+		bewitchment = FabricLoader.getInstance().isModLoaded("bewitchment");
+	}
+	
+	public static boolean isLibhudLoaded() {
+		return libhud;
+	}
+	
+	public static boolean isFabricApiLoaded() {
+		return fabricApi;
+	}
+	
+	public static boolean isAppleskinLoaded() {
+		return appleskin;
+	}
+	
+	public static boolean isSpectrumLoaded() {
+		return spectrum;
+	}
+	
+	public static boolean isRaisedLoaded() {
+		return raised;
+	}
+	
+	public static boolean isBewitchmentLoaded() {
+		return bewitchment;
+	}
+}
