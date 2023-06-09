66ce06c6a40fc2bb93b290905a0c44e791bb65e3
Cheos <cheoscrafter@gmail.com>
2023-06-09 13:10:15 +0200

    [fabric] support appleskin 1.20 + bump modmenu

--- fabric-1.20/src/main/java/dev/cheos/armorpointspp/compat/AppleskinSafeAccess.java
+++ fabric-1.20/src/main/java/dev/cheos/armorpointspp/compat/AppleskinSafeAccess.java
@@ -6,11 +6,11 @@ import squeek.appleskin.client.HUDOverlayHandler;
 public class AppleskinSafeAccess {
 	public static void handlerOnPreRender(GuiGraphics graphics) {
 		if (HUDOverlayHandler.INSTANCE != null)
-			HUDOverlayHandler.INSTANCE.onPreRender(graphics.pose()); // TODO outdated
+			HUDOverlayHandler.INSTANCE.onPreRender(graphics);
 	}
 	
 	public static void handlerOnRender(GuiGraphics graphics) {
 		if (HUDOverlayHandler.INSTANCE != null)
-			HUDOverlayHandler.INSTANCE.onRender(graphics.pose()); // TODO outdated
+			HUDOverlayHandler.INSTANCE.onRender(graphics);
 	}
 }
